5e89fa95a6e0fbfd53962df07c1600285c580b99
Fene-87 <fenekayas87@gmail.com>
2023-08-21 15:18:28 +0300

    Create auth module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [NinjasModule],
+  imports: [NinjasModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
