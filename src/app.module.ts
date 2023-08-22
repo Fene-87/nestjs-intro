aa40769503d95160118261907ed3d7de1c70a86d
Fene-87 <fenekayas87@gmail.com>
2023-08-22 14:34:10 +0300

    Create connection to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
 import { AuthModule } from './auth/auth.module';
+import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [NinjasModule, AuthModule],
+  imports: [
+    NinjasModule,
+    AuthModule,
+    BookModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
